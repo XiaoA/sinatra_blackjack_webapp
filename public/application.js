bd002a9907543ac2845201d8b2b33fb561e7495e
Andrew Buckingham <abuckingham99@gmail.com>
2015-03-02 23:03:26 -0600

    Add 'player_hit' function to 'application.js'

--- public/application.js
+++ public/application.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    player_hits();
+    player_stands();
+});
+
+function player_hits() {
     $(document).on("click", "form#hit_form input", function(){
 	alert("Player hits!");
 	$.ajax({
@@ -10,5 +15,21 @@ $(document).ready(function() {
 
 	return false;
     });
-});
+};
+
+function player_stands() {
+    $(document).on("click", "form#stand_form input", function(){
+	alert("Player stands!");
+	$.ajax({
+	    type: "POST",
+	    url: "/game/player/stand"
+	}).done(function(msg) {
+	    $('#game').replaceWith(msg);
+	});
+
+	return false;
+    });
+};
+
+
 
